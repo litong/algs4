89c779d9cdf90c8b5de70df57fdf88b286ecec17
litong <masonlitong@gmail.com>
2021-09-01 17:04:30 +0800

    feat: complete exercise 1.1.1

---
+++ src/main/java/dev/litong/algs4/chapter01/section01/Exercise01.java
@@ -0,0 +1,11 @@
+package dev.litong.algs4.chapter01.section01;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise01 {
+  public static void main(String[] args) {
+    StdOut.println("a: " + ((0 + 15) / 2));
+    StdOut.println("b: " + (2.0e-6 * 100000000.1));
+    StdOut.println("c: " + (true && false || true && true));
+  }
+}
