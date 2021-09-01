50607a8d2e5b12fd9fcd1ed9f00f6977e4db3c46
litong <masonlitong@gmail.com>
2021-09-01 17:06:52 +0800

    feat: complete exercise 1.1.2

---
+++ src/main/java/dev/litong/algs4/chapter01/section01/Exercise02.java
@@ -0,0 +1,12 @@
+package dev.litong.algs4.chapter01.section01;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise02 {
+  public static void main(String[] args) {
+    StdOut.println("a: " + ((1 + 2.236) / 2)); // a: 1.618 double
+    StdOut.println("b: " + (1 + 2 + 3 + 4.0)); // b: 10.0 double
+    StdOut.println("c: " + (4.1 >= 4)); // c: true boolean
+    StdOut.println("d: " + (1 + 2 + "3")); // d: 33 String
+  }
+}
