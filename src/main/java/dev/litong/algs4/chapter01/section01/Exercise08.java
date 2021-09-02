6cfae75ccf9182546c7d5611f27b2f53ba75272d
litong <masonlitong@gmail.com>
2021-09-02 13:21:16 +0800

    feat: complete exercise 1.1.8

---
+++ src/main/java/dev/litong/algs4/chapter01/section01/Exercise08.java
@@ -0,0 +1,9 @@
+package dev.litong.algs4.chapter01.section01;
+
+public class Exercise08 {
+  public static void main(String[] args) {
+    System.out.println('b');
+    System.out.println('b' + 'c');
+    System.out.println((char) ('a' + 4));
+  }
+}
